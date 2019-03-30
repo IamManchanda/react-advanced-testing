ab7ea1931d6bedb21f5cfc08a3ec499e2643a65c
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-03-30 15:46:04 +0530

    Chore & UI: Basic markup changes including basic grid ui logic!

--- src/views/app.js
+++ src/views/app.js
@@ -2,19 +2,16 @@ import React, { Fragment } from 'react';
 import CommentBox from 'views/components/comment-box/comment-box';
 import CommentList from 'views/components/comment-list/comment-list';
 
-const App = () => {
-  return (
-    <Fragment>
-      <div className="grid-container fluid">
-        <div className="grid-x grid-padding-x grid-padding-y">
-          <div className="cell medium-3">
-            <CommentBox />
-            <CommentList />
-          </div>
+const App = () => 
+  <Fragment>
+    <div className="grid-container fluid">
+      <div className="grid-x grid-padding-x grid-padding-y">
+        <div className="cell medium-6 large-3">
+          <CommentBox />
+          <CommentList />
         </div>
       </div>
-    </Fragment>
-  );
-};
+    </div>
+  </Fragment>;
 
 export default App;
