b6411c68955a7eded7834ac776d04831ab6d4c56
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-03-30 15:34:03 +0530

    Refactor comment box from functional to class based component
    Instead of using hooks for state we are using class based components because we need to create class based higher order components!

--- src/views/components/comment-box/comment-box.js
+++ src/views/components/comment-box/comment-box.js
@@ -1,39 +1,50 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { handleSaveComment, handleFetchComments } from 'store/actions';
 
-const CommentBox = ({ handleSaveComment, handleFetchComments }) => {
-  const [comment, setComment] = useState('');
-  const handleChange = (event) => {
-    setComment(event.target.value);
+const CommentBox = class extends Component {
+  state = {
+    comment: '',
   };
-  const handleSubmit = (event) => {
+
+  handleChange = (event) => {
+    this.setState({ comment: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    const { comment } = this.state;
+    const { handleSaveComment } = this.props;
     event.preventDefault();
     handleSaveComment(comment);
-    setComment('');
+    this.setState({ comment: '' });
   };
-  return (
-    <form onSubmit={ handleSubmit }>
-      <h4>Your Comment</h4>
-      <textarea
-        value={ comment }
-        onChange={ handleChange }
-        placeholder="Add a Comment" 
-      />
-      <button
-        type="submit"
-        className="button primary radius bordered shadow"
-        id="save-comments">
-        Submit Comment
-      </button>
-      <button
-        type="button"
-        className="button primary radius bordered shadow"
-        id="fetch-comments" 
-        onClick={ handleFetchComments }>Fetch Comments</button>
-    </form>
-  );
+
+  render() {
+    const { comment } = this.state;
+    const { handleFetchComments } = this.props;
+    return (
+      <form onSubmit={ this.handleSubmit }>
+        <h4>Your Comment</h4>
+        <textarea
+          value={ comment }
+          onChange={ this.handleChange }
+          placeholder="Add a Comment" 
+        />
+        <button
+          type="submit"
+          className="button primary radius bordered shadow"
+          id="save-comments">
+          Submit Comment
+        </button>
+        <button
+          type="button"
+          className="button primary radius bordered shadow"
+          id="fetch-comments" 
+          onClick={ handleFetchComments }>Fetch Comments</button>
+      </form>
+    );
+  }
 };
 
 CommentBox.defaultProps = {
